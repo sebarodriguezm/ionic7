eae4aef3167411db61cbd6a14cba6464349dc54c
sebarodriguezm <rodriguezmunoz.seba@gmail.com>
2023-07-02 10:49:39 -0400

    language en menu

--- ionic7/src/app/pages/menu/menu.page.ts
+++ ionic7/src/app/pages/menu/menu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LanguageService } from 'src/app/providers/languages.service';
 
 @Component({
   selector: 'app-menu',
@@ -13,9 +15,45 @@ export class MenuPage implements OnInit {
     { title: 'Registro', url: '/menu/register', icon: 'add-outline' },
     
  ];
-  constructor() { }
+ public otherLang = '';
 
-  ngOnInit() {
-  }
+ private _otherLang = '';
+ private suscribeLang: Subscription = new Subscription;
+
+ constructor(
+   private langService: LanguageService
+ ) {}
+
+ ngOnInit() {
+   this.langService.initialize();
+
+   this.setOtherLang();
+
+  this.suscribeLang = this.langService.languageChange$
+      .subscribe(() => {
+        this.setOtherLang();
+      });
+ }
+
+ ngOnDestroy() {
+   this.suscribeLang?.unsubscribe();
+ }
+
+ // Se obtiene el idioma no seleccionado
+ setOtherLang() {
+   // se obtiene el idioma no seleccionado
+   let nonSelected = this.langService.getNonSelectedLanguage();
+   if (nonSelected.length) {
+     this._otherLang = nonSelected[0];
+     this.otherLang = this._otherLang.toUpperCase();
+   }
+ }
+
+ // Se cambia el idioma
+ changeLanguage() {
+   this.langService.setLanguage(this._otherLang);
+ }
+
+
 
 }
