dd2944267d7bb30c57e8e8e2aacbe07108a0d3e7
sebarodriguezm <rodriguezmunoz.seba@gmail.com>
2023-10-18 20:07:41 -0300

    android

---
+++ android/app/src/main/java/cl/appcompras/app/MainActivity.java
@@ -0,0 +1,5 @@
+package cl.appcompras.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'cl.app-compras.app',
+  appId: 'cl.appcompras.app',
   appName: 'app-compras',
   webDir: 'www',
   server: {
